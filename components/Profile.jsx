9b42d6d0459aecedb6929472228efe53aa9c4126
Fikri Ramadianto <ramadiantofikri@gmail.com>
2023-05-21 19:37:55 +0700

    handle CTA

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,1 +1,30 @@
-export default function Profile() {}
+import PromptCard from "./PromptCard";
+
+export default function Profile({
+  name,
+  description,
+  data,
+  handleEdit,
+  handleDelete,
+}) {
+  return (
+    <>
+      <section className="w-full">
+        <h1 className="head_text text-left">
+          <span className="blue_gradient"> {name} Profile</span>
+        </h1>
+        <p className="desc text-left">{description}</p>
+        <div className="mt-16 prompt_layout">
+          {data.map((post) => (
+            <PromptCard
+              key={post._id}
+              post={post}
+              handleEdit={() => handleEdit && handleEdit(post)}
+              handleDelete={() => handleDelete && handleDelete(post)}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
